ee1a3e85def968f2fd0fb4f415c8cd84dbb8c502
JosephSLG2011 <josephlunaguerrero2011@gmail.com>
2026-02-21 06:28:48 -0500

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) "$@" O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
